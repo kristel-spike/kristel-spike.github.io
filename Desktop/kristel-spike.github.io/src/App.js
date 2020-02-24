503a24ca4951c648f23f80c95c0ed489b3a18cd7
Kristel B <kristelb@macbook-air.home>
2020-02-23 22:29:10 -0500

    modified App.js to say Kristel

--- Desktop/kristel-spike.github.io/src/App.js
+++ Desktop/kristel-spike.github.io/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Kristel and I'm going to try my best to master React!
         </p>
         <a
           className="App-link"
