915508a1960faa7fb3c757eddcc80cff23dc11fe
kristel-spike <kristelb@macbook-air.home>
2020-03-25 01:25:30 -0400

    drag capability files

---
+++ Desktop/hw3-master/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import DesignContainer from './components/DesignContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <DesignContainer/>
+    </div>
+  );
+}
+
+export default App;
