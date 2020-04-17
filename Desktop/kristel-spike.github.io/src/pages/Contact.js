a76f6540919d0cbfe570faae5c06fb0bb8137c1f
kristel-spike <kristelb@macbook-air.home>
2020-04-17 00:02:52 -0400

    finished up contact page and form for the most part

--- Desktop/kristel-spike.github.io/src/pages/Contact.js
+++ Desktop/kristel-spike.github.io/src/pages/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react';
-// import{BrowserRouter as Router, Switch, Route, Link} from "./node_modules/react-router-dom"
+// import{BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import Title from '../components/Title';
+import Content from '../components/Content';
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class Contact extends React.Component{
 
@@ -15,10 +19,52 @@ class Contact extends React.Component{
         }
     }
 
+    handleChange=(event) => {
+        const target = event.target;
+        const value = event.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]:value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true
+        })
+    }
+
     render(){
         return(
             <div>
                 <Title title={this.props.title}/>
+
+                <Content>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group className="border border-dark">
+                            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
+                        </Form.Group>
+                        <Form.Group className="border border-dark">
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
+                        </Form.Group>
+                        <Form.Group className="border border-dark">
+                            <Form.Label htmlFor="message">Message</Form.Label>
+                            <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange}/>
+                        </Form.Group>
+
+                        <Button className="d-inline-block" variant="primary" type="submit" diabled={this.state.disabled}>
+                            Send
+                        </Button>
+
+                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+                    </Form>
+                </Content>
             </div>
         )
     }
