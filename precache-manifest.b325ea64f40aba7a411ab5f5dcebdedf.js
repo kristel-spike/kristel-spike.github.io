02546d0de6f9306d2872e500a63280f2e850a686
kristel-spike <kristelb@macbook-air.home>
2020-04-19 21:41:06 -0400

    Updates

---
+++ precache-manifest.b325ea64f40aba7a411ab5f5dcebdedf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23bbd441a1a79f7fbfd8543e46fb191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1de56811f7b27492cef",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "817ca2bc575751e7ed83",
+    "url": "/static/css/main.2c36c26b.chunk.css"
+  },
+  {
+    "revision": "e1de56811f7b27492cef",
+    "url": "/static/js/2.9bb26840.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9bb26840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817ca2bc575751e7ed83",
+    "url": "/static/js/main.d4981357.chunk.js"
+  },
+  {
+    "revision": "6ea4234495122745f6dc",
+    "url": "/static/js/runtime-main.6c14371b.js"
+  },
+  {
+    "revision": "729dca08ee8a6471e5ddd506a5da7c63",
+    "url": "/static/media/behance.729dca08.jpg"
+  },
+  {
+    "revision": "3010a1e6a62133baabb25c4e721736e6",
+    "url": "/static/media/circles.3010a1e6.jpg"
+  },
+  {
+    "revision": "296a0b6317112896d3bc103d5eb9531c",
+    "url": "/static/media/contact.296a0b63.jpg"
+  },
+  {
+    "revision": "9f92baaa05a0da2099cffc924cb78478",
+    "url": "/static/media/github.9f92baaa.jpg"
+  },
+  {
+    "revision": "e0bc1146efc80ab07c9c63404d57185b",
+    "url": "/static/media/insta1.e0bc1146.jpg"
+  },
+  {
+    "revision": "1e8000504b62966938e6c644cb839699",
+    "url": "/static/media/k.1e800050.png"
+  },
+  {
+    "revision": "83d7edbe5d75e3f130587d90a255172f",
+    "url": "/static/media/portfolio.83d7edbe.jpg"
+  },
+  {
+    "revision": "abe521cdf04310d71cb8225d0942ec7a",
+    "url": "/static/media/spike.abe521cd.jpg"
+  }
+]);
